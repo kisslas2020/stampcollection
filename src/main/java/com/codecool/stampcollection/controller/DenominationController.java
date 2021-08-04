96f5e860ad2e7650c1907a055ca1ce24ab4ce7ed
kisslas2020 <pinewood82@gmail.com>
2021-08-04 14:35:01 +0200

    denomination exception handling with Problem

--- src/main/java/com/codecool/stampcollection/controller/DenominationController.java
+++ src/main/java/com/codecool/stampcollection/controller/DenominationController.java
@@ -3,16 +3,23 @@ package com.codecool.stampcollection.controller;
 import com.codecool.stampcollection.DTO.DTOMapper;
 import com.codecool.stampcollection.DTO.DenominationDTO;
 import com.codecool.stampcollection.assembler.DenominationModelAssembler;
+import com.codecool.stampcollection.exception.DenominationNotFoundException;
 import com.codecool.stampcollection.model.Denomination;
 import com.codecool.stampcollection.model.Stamp;
 import com.codecool.stampcollection.service.DenominationService;
 import com.codecool.stampcollection.service.StampService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/denomination")
@@ -62,5 +69,17 @@ public class DenominationController {
         service.delete(id);
     }
 
-
+    @ExceptionHandler(DenominationNotFoundException.class)
+    private ResponseEntity<Problem> handleNotFound(DenominationNotFoundException dnfe) {
+        Problem problem = Problem.builder()
+                .withType(URI.create("/api/denomination/denomination-not-found"))
+                .withTitle("not found")
+                .withStatus(Status.NOT_FOUND)
+                .withDetail(dnfe.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
+    }
 }
--- src/main/java/com/codecool/stampcollection/exception/DenominationNotFoundException.java
+++ src/main/java/com/codecool/stampcollection/exception/DenominationNotFoundException.java
@@ -1,8 +1,20 @@
 package com.codecool.stampcollection.exception;
 
-public class DenominationNotFoundException extends RuntimeException{
+import org.zalando.problem.AbstractThrowableProblem;
+import org.zalando.problem.Status;
+import org.zalando.problem.StatusType;
+
+import javax.annotation.Nullable;
+import java.net.URI;
+
+public class DenominationNotFoundException extends AbstractThrowableProblem {
 
     public DenominationNotFoundException(Long id) {
-        super("Could not find denomination with id: " + id);
+        super(
+                URI.create("/api/denomination/denomination-not-found"),
+                "not found",
+                Status.NOT_FOUND,
+                String.format("Denomination with id %d not found", id)
+        );
     }
 }
