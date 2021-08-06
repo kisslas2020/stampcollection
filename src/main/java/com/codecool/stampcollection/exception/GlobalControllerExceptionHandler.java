7d9da53c945dbc75ca1332d325113d1ea4817c47
kisslas2020 <pinewood82@gmail.com>
2021-08-06 14:50:10 +0200

    removing advice classes

--- src/main/java/com/codecool/stampcollection/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/codecool/stampcollection/exception/GlobalControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.zalando.problem.Problem;
@@ -15,16 +16,17 @@ import java.net.URI;
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Problem> handleJsonParseError(HttpMessageNotReadableException hmne) {
+    public ResponseEntity<Problem> handleJsonParseError(HttpMessageNotReadableException exception) {
         Problem problem = Problem.builder()
                 .withType(URI.create("/api/json-parse-error"))
                 .withTitle("JSON parse error")
                 .withStatus(Status.BAD_REQUEST)
-                .withDetail(hmne.getCause().getLocalizedMessage().split("\\n")[0])
+                .withDetail(exception.getCause().getLocalizedMessage().split("\\n")[0])
                 .build();
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .contentType(MediaType.APPLICATION_PROBLEM_JSON)
                 .body(problem);
     }
+    
 }
